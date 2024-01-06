53f8b2068006545cb4b8f7625ae4ff99545bd7f9
shale <slucas001@citymail.cuny.edu>
2024-01-06 02:02:37 -0500

    removed the matrix ops

---
+++ matrix/matrix.h
@@ -0,0 +1,22 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+// Matrix Structure
+typedef struct matrix {
+	int rows;
+	int cols; 
+	double **entries; 
+} matrix; 
+
+// Matrix function prototypes
+matrix * init_matrix (int rows, int cols); 
+matrix * fill_matrix (matrix *m, double num);
+matrix * copy_matrix (matrix *m);
+matrix * flatten_matrix (matrix *m, ...); 
+
+void print_matrix (matrix *m); 
+void free_matrix (matrix *m);
+void randomize_matrix (matrix *m, int n);  
+
+
+#endif // matrix.h
