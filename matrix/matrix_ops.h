d3275052556c6b34155b733627c80bb1f4380e85
shale <slucas001@citymail.cuny.edu>
2024-01-06 02:02:56 -0500

    put matrix ops here

---
+++ matrix/matrix_ops.h
@@ -0,0 +1,10 @@
+#include "matrix.h"
+
+
+// Matrix operations
+matrix * dot_prodcut (matrix *m); 
+matrix * add (matrix *m_one, matrix *m_two);
+matrix * subtract (matrix *m_one, matrix *m_two); 
+matrix * transpose (matrix *m); 
+matrix * multiply (matrix *m_one, matrix *m_two); 
+matrix * scalar_mult(double scalar, matrix *m); 
