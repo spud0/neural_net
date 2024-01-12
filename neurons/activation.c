efa8b11a33bb7795bc8d5b476c6f8b74a35bfe68
shale <slucas001@citymail.cuny.edu>
2024-01-12 00:06:14 -0500

    implemented sigmoid function

--- neurons/activation.c
+++ neurons/activation.c
@@ -1,9 +1,10 @@
-
+#include <math.h>
 #include "activation.h"
-#include "../matrix/matrix.h"
+#include "../matrix/matrix_ops.h"
 
 
-double sigmoid (double input); 
+double sigmoid (double input) {
+	return 1.0 / (1 + exp(-1 * input); 
 
 matrix *derivative_sigmoid (matrix *m); 
 matrix *softmax (matrix *m);
