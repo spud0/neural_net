cf236ce20a22d52544dfbe4f2a84f2ab4a5f29d2
shale <slucas001@citymail.cuny.edu>
2024-01-22 23:46:06 -0500

    implemented derivative_sigmoid and added missing curly brace

--- neurons/activation.c
+++ neurons/activation.c
@@ -5,6 +5,22 @@
 
 double sigmoid (double input) {
 	return 1.0 / (1 + exp(-1 * input); 
+}
 
-matrix *derivative_sigmoid (matrix *m); 
+
+matrix *derivative_sigmoid (matrix *m) {
+	// 
+	matrix * m_ones = init_matrix(m->rows, m->cols); 
+	m_ones = matrix_fill(m_ones , 1); 
+
+	matrix *subtracted = subtract(m_ones, m); 
+	matrix *mult = multiply(m, subtracted); 
+	
+	free_matrix(m_ones); 
+	free_matrix(subtracted);
+	return mult;
+}
+
+
+
 matrix *softmax (matrix *m);
