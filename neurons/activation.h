13dacdfbe839d400b527f76f77a1c178d752e585
shale <slucas001@citymail.cuny.edu>
2024-02-05 23:34:40 -0500

    fixed gitignore to reflect new directory structure

--- neurons/activation.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACTIVATION_H
-#define ACTIVATION_H
-
-double sigmoid (double input); 
-
-matrix *derivative_sigmoid (matrix *m); 
-matrix *softmax (matrix *m);
-
-#endif 
--- tests/test.c
+++ tests/test.c
@@ -2,36 +2,16 @@
 #include "../matrix/matrix_ops.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
-	//	matrix *m = init_matrix(5, 6); 
-
-	matrix *m1 = init_matrix(3, 3); 
-	matrix *m2 = init_matrix(3, 3); 
-	matrix *m3 = init_matrix(3, 3); 
-	matrix *m4 = init_matrix(3, 3); 
-
-	m1 = fill_matrix(m1, 20.0);
-	print_matrix(m1);
-
-	m2 = fill_matrix(m2, -6.834); 
-	matrix *m5 = add(m1, m2); 
+	matrix *m = init_matrix(3, 3); 	
+	randomize_matrix(m, 6); 
+	print_matrix(m);	
+	free_matrix(m); 
 	
-	m3 = fill_matrix(m3, -11.43); 
-	m4 = fill_matrix(m4, -0.0056);
-
-	m4 = add(m3, m4); 
-	m2 = subtract(m1, m4); 
-
-	print_matrix(m4); 
-	printf("\n"); 
-	print_matrix(m2); 
-
-	printf("\n"); 
-	print_matrix(m5); 
-			
-	return 0; 
-
+	
+	return 0;
 }
 
 
