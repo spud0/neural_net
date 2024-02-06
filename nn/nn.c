a5125eb209e5fdaf5aae1bf663eb88547f8e946d
shale <slucas001@citymail.cuny.edu>
2024-02-05 23:30:35 -0500

    continued implementing print_network, and init_networkontinued implementing print_mat, & init_network, not done

--- nn/nn.c
+++ nn/nn.c
@@ -1,15 +1,35 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-
 #include "nn.h"
 #include "../matrix/matrix_ops.h"
 #include "../neurons/activation.h"
 
 
 
-void print_network(nn * network){
+nn * init_network(int input, int hidden, int output, double learning_rate){
 
+	nn * network = malloc(sizeof(nn)); 
+	
+	network->input = input; 
+	network->hidden = hidden;
+	network->output = output; 
+	network->learning_rate = learning_rate; 	
+
+	return network; 
+}
+
+
+
+void print_network(nn * network){
+	
+	printf("Number of inputs: %d\n", network->input); 	
+	printf("Number of outputs: %d\n", network->output); 
+	printf("The learning rate: %d\n", network->learning_rate); 
+	
+	// The weights	
+	print_matrix(network->hidden_weights); 
+	print_matrix(network->output_weights); 
 }
 
 
